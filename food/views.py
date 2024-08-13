04f4844c4510652f3c89624812ec841c8326b643
Devansh Sirohi <honeysirohi2438@gmail.com>
2024-08-13 21:03:21 +0530

    added templates

--- food/views.py
+++ food/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the food index.")
+    items=Item.objects.all()
+    template=loader.get_template('food/index.html')
+    return HttpResponse(template.render({
+        'items':items
+    },request))
+    # return HttpResponse(items)
+
+def details(request,item_id):
+    item=Item.objects.get(pk=item_id)
+    context={
+        'item':item
+    }
+    return render(request,'food/details.html',context)
