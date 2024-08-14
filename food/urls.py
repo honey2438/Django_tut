f7b9dfdfc37f041a59b2b0dbfd21f2289e95bee4
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2024-08-14 18:47:26 +0530

    added crud operations

--- food/urls.py
+++ food/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("",views.index,name='index'),
     path("<int:item_id>/",views.details,name="details"),
+    path("add/",views.create_item,name="add"),
+    path("update/<int:id>/",views.update_item,name="update"),
+    path("delete/<int:id>/",views.delete_item,name="delete")
 ]
